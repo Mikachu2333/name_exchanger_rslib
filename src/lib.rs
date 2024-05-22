c12a806bc528ccc5ff355bacaac837b4d1d86bb3
Mikachu2333 <mikachu.23333@zohomail.com>
2024-05-22 14:50:57 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -217,14 +217,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn it_works()->u8{
         //1 no exist
         //2 not absolte
         //3 no permission
         //4 already exist
-        process_input::change_name::exchange(
+        exchange_input(
             String::from(r"PATH1"),
             String::from(r"PATH2"),
-        );
+        )
     }
 }
